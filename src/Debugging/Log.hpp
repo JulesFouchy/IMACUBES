5ba89e34b7b9a396400133eb1cbc80b00e603823
Jules Fouchy <jules.fouchy@ntymail.com>
2019-11-08 22:34:25 +0100

    no reason for a class for Log, a namespace is better

--- src/Debugging/Log.hpp
+++ src/Debugging/Log.hpp
@@ -4,17 +4,13 @@
 
 #include "glm/glm.hpp"
 
-class Log {
-public:
-	static void Initialize();
+namespace Log {
+	void Initialize();
 
-	static void separationLine();
-	static void log(glm::vec2 v);
-	static void log(glm::vec3 v);
-	static void log(glm::vec4 v);
-	static void log(glm::mat3x3 mat);
-	static void log(glm::mat4x4 mat);
-private:
-	Log();
-	~Log();
+	void separationLine();
+	void log(glm::vec2 v);
+	void log(glm::vec3 v);
+	void log(glm::vec4 v);
+	void log(glm::mat3x3 mat);
+	void log(glm::mat4x4 mat);
 };
