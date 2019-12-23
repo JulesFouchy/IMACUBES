3ec669bccefe0a21712a22d925a48524f3476836
Jules Fouchy <jules.fouchy@ntymail.com>
2019-12-23 13:58:39 +0100

    Fix empty implementation

--- src/GUI/FileBrowser_LinuxImpl.cpp
+++ src/GUI/FileBrowser_LinuxImpl.cpp
@@ -6,10 +6,12 @@
 
 std::string FileBrowser::openfilename(const char* filter) {
 	spdlog::warn("Sorry, file explorer not implemented for Linux yet ! ");
+	return "";
 }
 
 std::string FileBrowser::savefilename(const char* filter) {
 	spdlog::warn("Sorry, file explorer not implemented for Linux yet ! ");
+	return "";
 }
 
 #endif //  __linux__
--- src/GUI/FileBrowser_MacImpl.cpp
+++ src/GUI/FileBrowser_MacImpl.cpp
@@ -6,10 +6,12 @@
 
 std::string FileBrowser::openfilename(const char* filter) {
 	spdlog::warn("Sorry, file explorer not implemented for Mac yet ! ");
+	return "";
 }
 
 std::string FileBrowser::savefilename(const char* filter) {
 	spdlog::warn("Sorry, file explorer not implemented for Mac yet ! ");
+	return "";
 }
 
 #endif //  __APPLE__
