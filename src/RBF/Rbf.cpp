bdbf2fdda17112951fdc839b1a913087f8f03279
Thomas <thomasgeslin91@hotmail.fr>
2019-12-23 16:47:10 +0100

    Debugging of the RBF files (WITH EIGEN)

--- src/RBF/Rbf.cpp
+++ src/RBF/Rbf.cpp
@@ -1,31 +1,31 @@
 #include "Rbf.hpp"
-
 #include <math.h>
 #include <imgui.h>
 
-double distance(glm::vec3 a, b) {
+double distance(glm::vec3 a, glm::vec3 b) {
 	return glm::distance(a, b);
 }
 
-double multiQuadra(glm::vec3 a,b){
-	return sqrt(1 + sqrt(pow(glm::distance(a,b),2));
+double multiQuadra(glm::vec3 a,glm::vec3 b){
+	return sqrt(1 + sqrt(pow(glm::distance(a,b),2)));
 }
 
-double invMultiQuadra(glm::vec3 a, b){
+double invMultiQuadra(glm::vec3 a, glm::vec3 b){
 	return  1 / pow(glm::distance(a,b),2) ;
 }
 
-double gaussian(glm::vec3 a, b){
+double gaussian(glm::vec3 a, glm::vec3 b){
 	return exp(-pow(glm::distance(a,b),2));
 
 }
-	
+
+
 Eigen::VectorXd calculOmega(const std::vector<glm::vec3> &anchorPts,const Eigen::VectorXd &valuesAtAnchorPts) {
 	Eigen::MatrixXd anchorMatrix(anchorPts.size(), anchorPts.size());
 
-	for (int i = 0, i < anchorPts.size(), ++i) {
-		for (int j = 0, j < anchorPts.size(), ++j) {
-			anchorMatrix[i, j] = gaussian(anchorPts[i], anchorPts[j]);
+	for (int i = 0; i < anchorPts.size(); ++i) {
+		for (int j = 0; j < anchorPts.size(); ++j) {
+			anchorMatrix(i, j) = gaussian(anchorPts[i], anchorPts[j]);
 		}
 	}
 
--- src/RBF/Rbf.hpp
+++ src/RBF/Rbf.hpp
@@ -5,8 +5,17 @@
 
 #include "Debugging/Log.hpp"
 
-double distance(glm::vec3 a, b);
-double multiQuadra(glm::vec3 a, b);
-double invMultiQuadra(glm::vec3 a, b);
-double gaussian(glm::vec3 a, b);
+double distance(glm::vec3 a, glm::vec3 b);
+double multiQuadra(glm::vec3 a, glm::vec3 b);
+double invMultiQuadra(glm::vec3 a, glm::vec3 b);
+double gaussian(glm::vec3 a, glm::vec3 b);
 Eigen::VectorXd calculOmega(const std::vector<glm::vec3>& anchorPts, const Eigen::VectorXd& valuesAtAnchorPts);
+
+class RBF{
+	RBF(std::vector<glm::vec3> anchorPts, Eigen::VectorXd valuesAtAnchorPts) {
+		omega = calculOmega(anchorPts, valuesAtAnchorPts);
+	}
+private:
+	std::vector <glm::vec3> anchorPts;
+	Eigen::VectorXd omega;
+};
