1a5cdcf4a94efe29d583a0e162dcba3ed76f3cdb
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-06 00:05:13 +0100

    Fix : forgot to compute cursor's position after switching positioning mode

--- src/App.cpp
+++ src/App.cpp
@@ -187,6 +187,7 @@ void App::onEvent(const SDL_Event& e) {
 				}
 				if (e.key.keysym.scancode == SDL_SCANCODE_TAB) {
 					m_cursor.switchCursorPositioner();
+					m_cursor.computePosition();
 				}
 				if (e.key.keysym.sym == 'z') {
 					m_cursor.translate(glm::ivec3(0, 0, -1));
