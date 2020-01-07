d7e8c27418eeede2d16ec5a21c9aa25271e95d32
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-07 12:44:15 +0100

    Simplify Tool_OvoidTwoCorners too

--- src/Tools/Tool_OvoidTwoCorners.cpp
+++ src/Tools/Tool_OvoidTwoCorners.cpp
@@ -4,27 +4,8 @@
 
 void Tool_OvoidTwoCorners::applyOnShape(std::function<void(const glm::ivec3 & pos)> whatToDoWithPos) {
 	computeBoundingBox();
-	int nbDimensions = (m_bbox.size().x != 0 ? 1 : 0)
-					 + (m_bbox.size().y != 0 ? 1 : 0)
-					 + (m_bbox.size().z != 0 ? 1 : 0);
-	if (nbDimensions == 3) {
-		for (const glm::ivec3& pos : m_bbox) {
-			if (glm::length((glm::vec3)(pos - m_bbox.center()) / (glm::vec3)m_bbox.size()) <= 0.5f)
-				whatToDoWithPos(pos);
-		}
-	}
-	else if (nbDimensions == 2) { // to prevent divisions by 0 when dividing by bbox.size()
-		int c0, c1;
-		if      (m_bbox.size().x == 0) { c0 = 1; c1 = 2; }
-		else if (m_bbox.size().y == 0) { c0 = 0; c1 = 2; }
-		else                           { c0 = 0; c1 = 1; }
-		for (const glm::ivec3& pos : m_bbox) {
-			if (MyMaths::NormalizedDistance2D(pos, m_bbox.center(), m_bbox.size(), c0, c1) <= 0.5f)
-				whatToDoWithPos(pos);
-		}
-	}
-	else {
-		for (const glm::ivec3& pos : m_bbox)
+	for (const glm::ivec3& pos : m_bbox) {
+		if (glm::length( ((glm::vec3)pos - m_bbox.centerFloat()) / (glm::vec3)m_bbox.size() ) <= 0.5f)
 			whatToDoWithPos(pos);
 	}
 }
